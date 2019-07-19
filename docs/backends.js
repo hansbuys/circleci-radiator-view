3fba6ce279132f00cd5dcf0bf0f52dec1d9bb022
Hans Buys <hans@autonomic.ai>
2019-07-19 10:15:08 -0400

    Add JS Linter and clean up JS code.

--- docs/backends.js
+++ docs/backends.js
@@ -14,36 +14,36 @@ Build type:
 */
 
 function buildBackend(settings, callback) {
-  var backend = circleBackend
+  const backend = circleBackend;
   if (settings.mode === 'travis') {
-    backend = travisBackend
+    backend = travisBackend;
   } else if (settings.mode === 'jenkins') {
-    backend = jenkinsBackend
+    backend = jenkinsBackend;
   }
-  var branchFilter = function(build) {
-    return settings.branch ? build.branch.match(settings.branch) : true
-  }
-  var repositoryFilter = function(build) {
+  const branchFilter = function(build) {
+    return settings.branch ? build.branch.match(settings.branch) : true;
+  };
+  const repositoryFilter = function(build) {
     return settings.repositories
       ? settings.repositories.split(',').includes(build.repository)
-      : true
-  }
+      : true;
+  };
 
   return function() {
     backend(settings, function(err, data) {
       if (err) {
-        return callback(err)
+        return callback(err);
       }
-      let builds = data.filter(repositoryFilter).filter(branchFilter)
+      let builds = data.filter(repositoryFilter).filter(branchFilter);
       builds = _.uniqBy(builds, function(b) {
-        return b.repository + b.branch
-      })
+        return b.repository + b.branch;
+      });
       builds = builds.sort(function(a, b) {
-        return a.started.getTime() - b.started.getTime()
-      })
-      callback(undefined, builds)
-    })
-  }
+        return a.started.getTime() - b.started.getTime();
+      });
+      callback(undefined, builds);
+    });
+  };
 }
 
 function backendOptions() {
@@ -51,69 +51,69 @@ function backendOptions() {
     circle: {
       name: 'Circle CI',
       url: 'https://circleci.com/api/v1/projects',
-      token: undefined
+      token: undefined,
     },
     travis: {
       name: 'Travis CI',
       url: 'https://api.travis-ci.com/repos',
-      token: undefined
+      token: undefined,
     },
     jenkins: {
       name: 'Jenkins CI',
       url: undefined,
-      token: undefined
-    }
-  }
+      token: undefined,
+    },
+  };
 }
 
-function httpRequest(url, handler /*, headers */) {
-  var request = new XMLHttpRequest()
-  var headers = arguments[2] || {}
-  request.open('GET', url, true)
+function httpRequest(url, handler, arguments) {
+  const request = new XMLHttpRequest();
+  const headers = arguments[0] || {};
+  request.open('GET', url, true);
   Object.keys(headers).forEach(function(headerName) {
-    request.setRequestHeader(headerName, headers[headerName])
-  })
+    request.setRequestHeader(headerName, headers[headerName]);
+  });
   request.onload = function() {
     if (request.status === 401 || request.status === 403) {
-      handler('Invalid API token (' + request.status + ' ' + request.responseText + ')')
+      handler('Invalid API token (' + request.status + ' ' + request.responseText + ')');
     } else if (request.status >= 200 && request.status < 400) {
       try {
-        var data = JSON.parse(request.responseText)
-        handler(undefined, data)
+        const data = JSON.parse(request.responseText);
+        handler(undefined, data);
       } catch (exc) {
-        console.log('Error fetching URL', url, request.responseText)
-        handler(exc)
+        console.log('Error fetching URL', url, request.responseText);
+        handler(exc);
       }
     } else {
-      handler('Error getting URL ' + url + ':' + request.status)
+      handler('Error getting URL ' + url + ':' + request.status);
     }
-  }
-  request.send()
+  };
+  request.send();
 }
 
-var travisBackend = function(settings, resultCallback) {
-  var travisRequest = function(url, cb) {
-    var handler = function(err, data) {
+const travisBackend = function(settings, resultCallback) {
+  const travisRequest = function(url, cb) {
+    const handler = function(err, data) {
       if (err) {
-        resultCallback(err)
+        resultCallback(err);
       } else {
-        cb(data)
+        cb(data);
       }
-    }
+    };
     httpRequest(url, handler, {
       Accept: 'application/vnd.travis-ci.2+json',
-      Authorization: 'token ' + settings.token
-    })
-  }
+      Authorization: 'token ' + settings.token,
+    });
+  };
 
-  var translateBuild = function(reponame, commits) {
-    var findCommit = function(commit) {
+  const translateBuild = function(reponame, commits) {
+    const findCommit = function(commit) {
       return commits.find(function(c) {
-        return c.id == commit
-      })
-    }
+        return c.id == commit;
+      });
+    };
     return function(b) {
-      var commit = findCommit(b.commit_id)
+      const commit = findCommit(b.commit_id);
       return {
         repository: reponame,
         branch: commit.branch,
@@ -122,197 +122,199 @@ var travisBackend = function(settings, resultCallback) {
         commit: {
           created: new Date(commit.committed_at),
           author: commit.author_name,
-          hash: commit.sha
-        }
-      }
-    }
-  }
+          hash: commit.sha,
+        },
+      };
+    };
+  };
 
-  var parseBuilds = function(repos) {
-    var responses = []
+  const parseBuilds = function(repos) {
+    const responses = [];
     repos.forEach(function(r) {
       travisRequest(settings.url + '/' + r.name + '/builds', function(data) {
-        var reponame = r.name.split('/')[1]
-        var builds = data.builds.map(translateBuild(reponame, data.commits))
-        responses.push(builds)
+        const reponame = r.name.split('/')[1];
+        const builds = data.builds.map(translateBuild(reponame, data.commits));
+        responses.push(builds);
         if (responses.length === repos.length) {
-          var result = responses.reduce(function(acc, item) {
-            return item.length > 0 ? acc.concat(item) : acc
-          }, [])
-          resultCallback(undefined, result)
+          const result = responses.reduce(function(acc, item) {
+            return item.length > 0 ? acc.concat(item) : acc;
+          }, []);
+          resultCallback(undefined, result);
         }
-      })
-    })
-  }
+      });
+    });
+  };
 
   travisRequest(settings.url, function(data) {
     parseBuilds(
-      data.repos.map(function(repo) {
-        return { id: repo.id, name: repo.slug }
-      })
-    )
-  })
-}
+        data.repos.map(function(repo) {
+          return {id: repo.id, name: repo.slug};
+        })
+    );
+  });
+};
 
-var circleBackend = function(settings, resultCallback) {
-  var url = settings.url + '?circle-token=' + settings.token
+const circleBackend = function(settings, resultCallback) {
+  const url = settings.url + '?circle-token=' + settings.token;
 
   httpRequest(
-    url,
-    function(err, data) {
-      if (err) {
-        return resultCallback(err)
+      url,
+      function(err, data) {
+        if (err) {
+          return resultCallback(err);
+        }
+        const builds = data.reduce(function(acc, repository) {
+          return acc.concat(
+              Object.keys(repository.branches).map(function(branchName) {
+                const branch = repository.branches[branchName];
+                const hasNeverBuilt = !branch.running_builds && !branch.recent_builds;
+                if (hasNeverBuilt) {
+                  console.warn('Repository', repository.reponame, 'has a branch named', branchName, 'that has never been built');
+                  return;
+                }
+                const buildIsRunning = branch.running_builds.length != 0;
+                let recent_builds = branch.recent_builds
+                if (recent_builds) {
+                  recent_builds = recent_builds.sort((a, b) => b.build_num - a.build_num)
+                }
+                var build = buildIsRunning ? branch.running_builds[0] : recent_builds[0]
+                const status = buildIsRunning ? build.status : build.outcome;
+                return {
+                  repository: repository.reponame,
+                  branch: branchName,
+                  started: new Date(build.pushed_at),
+                  state: status,
+                  commit: {
+                    created: new Date(build.pushed_at),
+                    author: null,
+                    hash: build.vcs_revision,
+                  },
+                };
+              })
+          );
+        }, []).filter(function(build) {
+          return !!build; 
+        });
+        resultCallback(undefined, builds);
+      },
+      {
+        Accept: 'application/json',
       }
-      var builds = data.reduce(function(acc, repository) {
-        return acc.concat(
-          Object.keys(repository.branches).map(function(branchName) {
-            var branch = repository.branches[branchName]
-            var hasNeverBuilt = !branch.running_builds && !branch.recent_builds
-            if (hasNeverBuilt) {
-              console.warn('Repository', repository.reponame, 'has a branch named', branchName, 'that has never been built')
-              return
-            }
-            var buildIsRunning = branch.running_builds.length != 0
-            let recent_builds = branch.recent_builds
-            if (recent_builds) {
-              recent_builds = recent_builds.sort((a, b) => b.build_num - a.build_num)
-            }
-            var build = buildIsRunning ? branch.running_builds[0] : recent_builds[0]
-            var status = buildIsRunning ? build.status : build.outcome
-            return {
-              repository: repository.reponame,
-              branch: branchName,
-              started: new Date(build.pushed_at),
-              state: status,
-              commit: {
-                created: new Date(build.pushed_at),
-                author: null,
-                hash: build.vcs_revision
-              }
-            }
-          })
-        )
-      }, []).filter(function (build) { return !!build })
-      resultCallback(undefined, builds)
-    },
-    {
-      Accept: 'application/json'
-    }
-  )
-}
+  );
+};
 
-var jenkinsBackend = function(settings, resultCallback) {
-  var jenkinsRequest = function(url, cb) {
-    var handler = function(err, data) {
+const jenkinsBackend = function(settings, resultCallback) {
+  const jenkinsRequest = function(url, cb) {
+    const handler = function(err, data) {
       if (err) {
-        resultCallback(err)
+        resultCallback(err);
       } else {
-        cb(data)
+        cb(data);
       }
-    }
-    var headers = {}
+    };
+    const headers = {};
     if (settings.token) {
-      headers.Authorization = 'Basic ' + window.btoa(settings.token)
+      headers.Authorization = 'Basic ' + window.btoa(settings.token);
     }
-    httpRequest(url, handler, headers)
-  }
+    httpRequest(url, handler, headers);
+  };
 
-  var findLastCommit = function(builds) {
+  const findLastCommit = function(builds) {
     if (!builds) {
-      return undefined
+      return undefined;
     }
-    var lastBuildWithCommits = builds.filter(function(b) {
-      return b.changeSets && b.changeSets.length > 0
-    })[0]
+    const lastBuildWithCommits = builds.filter(function(b) {
+      return b.changeSets && b.changeSets.length > 0;
+    })[0];
     if (!lastBuildWithCommits) {
-      return undefined
+      return undefined;
     }
-    var lastCommits = lastBuildWithCommits.changeSets[0].items.map(function(item) {
+    const lastCommits = lastBuildWithCommits.changeSets[0].items.map(function(item) {
       return {
         created: new Date(item.timestamp),
         author: item.author.fullName,
-        hash: item.commitId
-      }
-    })
-    return lastCommits[lastCommits.length - 1]
-  }
+        hash: item.commitId,
+      };
+    });
+    return lastCommits[lastCommits.length - 1];
+  };
 
-  var findBuildReason = function(builds) {
+  const findBuildReason = function(builds) {
     if (!builds) {
-      return undefined
+      return undefined;
     }
-    var lastBuildWithActions = builds.filter(function(b) {
-      return b.actions && b.actions.length > 0
-    })[0]
+    const lastBuildWithActions = builds.filter(function(b) {
+      return b.actions && b.actions.length > 0;
+    })[0];
     if (!lastBuildWithActions) {
-      return undefined
+      return undefined;
     }
-    var reason = lastBuildWithActions.actions
-      .filter(function(a) {
-        return a._class == 'hudson.model.CauseAction'
-      })
-      .reduce(function(acc, a) {
-        return acc.concat(
-          a.causes.map(function(c) {
-            return c.shortDescription
-          })
-        )
-      }, [])
-      .join(';')
+    const reason = lastBuildWithActions.actions
+        .filter(function(a) {
+          return a._class == 'hudson.model.CauseAction';
+        })
+        .reduce(function(acc, a) {
+          return acc.concat(
+              a.causes.map(function(c) {
+                return c.shortDescription;
+              })
+          );
+        }, [])
+        .join(';');
 
     return {
-      author: reason
-    }
-  }
+      author: reason,
+    };
+  };
 
-  var findResponsible = function(job) {
+  const findResponsible = function(job) {
     if (!job.actions) {
-      return undefined
+      return undefined;
     }
-    var contributorActions = job.actions.filter(function(action) {
-      return action.contributor || action.contributorDisplayName
-    })
-    var contributor = contributorActions[0]
+    const contributorActions = job.actions.filter(function(action) {
+      return action.contributor || action.contributorDisplayName;
+    });
+    const contributor = contributorActions[0];
     if (!contributor) {
-      return undefined
+      return undefined;
     }
     return {
       created: new Date(job.timestamp),
-      author: contributor.contributorDisplayName || contributor.contributor
-    }
-  }
+      author: contributor.contributorDisplayName || contributor.contributor,
+    };
+  };
 
-  var url =
+  const url =
     settings.url +
-    '/api/json?depth=4&tree=name,url,jobs[name,url,jobs[name,url,actions[contributor,contributorDisplayName,contributorEmail],buildable,builds[result,building,actions[causes[shortDescription]],changeSets[items[author[fullName],timestamp,commitId]],timestamp]]]'
+    '/api/json?depth=4&tree=name,url,jobs[name,url,jobs[name,url,actions[contributor,contributorDisplayName,contributorEmail],buildable,builds[result,building,actions[causes[shortDescription]],changeSets[items[author[fullName],timestamp,commitId]],timestamp]]]';
   jenkinsRequest(url, function(data) {
-    var builds = data.jobs.reduce(function(acc, project) {
+    const builds = data.jobs.reduce(function(acc, project) {
       return acc.concat(
-        project.jobs.reduce(function(acc, job) {
-          if (!job.buildable) {
-            return acc
-          }
-          var build = job.builds[0] || {}
-          var result = 'failed'
-          if (build.building) {
-            result = 'started'
-          } else if (build.result === 'ABORTED') {
-            result = 'canceled'
-          } else if (build.result === 'SUCCESS') {
-            result = 'success'
-          }
+          project.jobs.reduce(function(acc, job) {
+            if (!job.buildable) {
+              return acc;
+            }
+            const build = job.builds[0] || {};
+            let result = 'failed';
+            if (build.building) {
+              result = 'started';
+            } else if (build.result === 'ABORTED') {
+              result = 'canceled';
+            } else if (build.result === 'SUCCESS') {
+              result = 'success';
+            }
 
-          return acc.concat({
-            repository: project.name,
-            branch: job.name,
-            started: new Date(build.timestamp),
-            state: result,
-            commit:
-              findLastCommit(job.builds) || findResponsible(job) || findBuildReason(job.builds)
-          })
-        }, [])
-      )
-    }, [])
-    resultCallback(undefined, builds)
-  })
-}
+            return acc.concat({
+              repository: project.name,
+              branch: job.name,
+              started: new Date(build.timestamp),
+              state: result,
+              commit:
+              findLastCommit(job.builds) || findResponsible(job) || findBuildReason(job.builds),
+            });
+          }, [])
+      );
+    }, []);
+    resultCallback(undefined, builds);
+  });
+};
